87d7f333afc4fe19c1f7735bfcb6287f19840207
Vovkulaka21 <Vovkulaka16@gmail.com>
2023-12-25 06:33:23 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,24 @@
+// TASK1
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+// // TASK2
+// import Statisctics from "./Statistics/Statistics"
+// import data from "../data/data.json"
+// // TASK3
+// import FriendList from "./FriendList/FriendList"
+// import friends from "../data/friends.json"
+// // TASK4
+// import TransactionHistory from "./TransactionHistory/TransactionHistory"
+// import transaction from "../data/transactions.json"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-    
-      React homework template
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
