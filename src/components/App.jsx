9a4241de9105e533c824fd5ecc11e0971a6fa508
Vovkulaka21 <Vovkulaka16@gmail.com>
2023-12-26 04:04:56 +0200

    Task4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,8 @@ import data from '../data/data.json';
 import FriendList from "./FriendList/FriendList"
 import friends from "../data/friends.json"
 // // TASK4
-// import TransactionHistory from "./TransactionHistory/TransactionHistory"
-// import transaction from "../data/transactions.json"
+import TransactionHistory from "./TransactionHistory/TransactionHistory"
+import transactions from "../data/transactions.json"
 
 export const App = () => {
   return (
@@ -28,6 +28,8 @@ export const App = () => {
       <Statisctics title="Upload stats" stats={data} />
 
       <FriendList friends={friends}/>
+
+      <TransactionHistory transactions={transactions} />;
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,28 @@
+import css from "./TransactionHistory.module.css"
+
+const TransactionHistory = ({ transactions }) => {
+
+  const table = transactions.map(transaction => (
+    <tr key={transaction.id}>
+      <td>{transaction.type}</td>
+      <td>{transaction.amount}</td>
+      <td>{transaction.currency}</td>
+    </tr>
+  ));
+
+  
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>{table}</tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
