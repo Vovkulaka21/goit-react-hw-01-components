0b636e614a90fa6553fa951b89495ed7c66f31ca
Vovkulaka21 <Vovkulaka16@gmail.com>
2023-12-26 13:04:57 +0200

    styles task4

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,7 +3,7 @@ import css from "./TransactionHistory.module.css"
 const TransactionHistory = ({ transactions }) => {
 
   const table = transactions.map(transaction => (
-    <tr key={transaction.id}>
+    <tr key={transaction.id} className={css.block}>
       <td>{transaction.type}</td>
       <td>{transaction.amount}</td>
       <td>{transaction.currency}</td>
@@ -14,7 +14,7 @@ const TransactionHistory = ({ transactions }) => {
   return (
     <table className={css.table}>
       <thead className={css.head}>
-        <tr>
+        <tr className={css.block}>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
