4bd9b1a7b82f5a7b17f4a8177ea71f87d99bfbf0
Vovkulaka21 <Vovkulaka16@gmail.com>
2023-12-26 12:05:51 +0200

    Random color

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,15 @@
 import css from './Statistics.module.css';
 
-
-
-
+const generateRandomColor = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red},${green},${blue})`;
+};
 
 const Statisctics = ({ title, stats = [] }) => {
   const statsList = stats.map(stat => (
-    <li key={stat.id} className={css.item}>
+    <li key={stat.id} className={css.item} style={{ backgroundColor: generateRandomColor() }}>
       <span>{stat.label}</span>
       <span>{stat.percentage}%</span>
     </li>
